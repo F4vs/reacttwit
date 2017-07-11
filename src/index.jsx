7b222c15365835e21ac48ea7088c6337296cd6a4
Felipe Valenzuela <favs92@gmail.com>
2017-07-11 17:26:30 -0600

    Loader de general de la app

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+// Importacion de componentes
+import App from './components/App'
+
+render(<App />, document.getElementById('root'))
