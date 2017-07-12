fea6dae78945938f7ea02cbeaa28755f38f51f65
Felipe Valenzuela <favs92@gmail.com>
2017-07-11 21:49:29 -0600

    Se modifico la estructura del header y se agregaron algunos estilos css

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 
+
+//Importacion de estilos css
+import style from './header.css'
+
 class Header extends Component {
   render () {
     return (
-      <header>
-        <h1>Este es el header</h1>
+      <header className={style.root}>
+        <h1 className={style.logo}>REACTTWIT</h1>
       </header>
     );
   }
